b22e33636c16b943888cd5d230a1375b2e2b4f8d
Alexandra Qi <66907386+rhythm0@users.noreply.github.com>
2024-04-03 20:50:45 -0500

    week 3

--- week3/dice.js
+++ week3/dice.js
@@ -4,6 +4,21 @@ const pickRandomNumber = function() {
 }
 
 // Your code goes here...
-
+function onDocumentLoaded() {
+  console.log("Document loaded") // debug
+  const link = document.querySelector("main a")//找到主要的a 不然就是index html
+  console.log(link) // debug
+  link.addEventListener("click",rollTheDice)
+}
+function rollTheDice(event) {
+  console.log("Rolling the dice") // debug
+  event.preventDefault();
+  let n1 = pickRandomNumber()
+  let n2 = pickRandomNumber()
+  let images = document.querySelectorAll("#dice img")
+  images[0].src = "images/" + n1 + ".png"
+  images[1].src = "images/" + n2 + ".png"
+}
+document.addEventListener("DOMContentLoaded", onDocumentLoaded)
 
 
--- week3/gps.js
+++ week3/gps.js
@@ -1,5 +1,13 @@
 // Your code goes here
 
+function onDocumentLoaded() {
+  document.querySelector("#get-location").addEventListener("click", function(e){
+    e.preventDefault();
+    navigator.geolocation.getCurrentPosition(setImagePosition)
+  })
+}
+document.addEventListener("DOMContentLoaded", onDocumentLoaded)
+
 // Update the map given a location object
 function setImagePosition(location) {
   console.log(location);
