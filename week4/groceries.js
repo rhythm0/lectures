02a0bbd7344da56b0ad1d00fe3f577da7e86f1c6
Jeff Cohen <cohen.jeff@gmail.com>
2024-04-10 15:31:06 -0500

    Week 4 at the break

--- week4/groceries.js
+++ week4/groceries.js
@@ -3,13 +3,26 @@
 // - the text input should be reset to a blank input field
 // - the cursor should be blinking in the text input again
 
+
 function setupListeners() {
   document.querySelector("form").addEventListener("submit", addItem)
 }
-
-setupListeners();
+document.addEventListener("DOMContentLoaded", setupListeners)
 
 function addItem(e) {
   e.preventDefault();
   console.log(e);
+
+  let textToAdd = document.querySelector("input").value
+  
+  let template = document.querySelector("#new-item-template")
+  let newItem = template.content.cloneNode(true)
+  // let newItem = document.createElement("li")
+  newItem.querySelector("li").textContent = textToAdd
+  let ul = document.querySelector("#groceries")
+  ul.appendChild(newItem)
+
+  document.querySelector("input").value = ""
+
+
 }
