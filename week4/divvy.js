ca8cd3c3068f8ad3b0f181db14e6c8b2b1b2e63f
Alexandra Qi <66907386+rhythm0@users.noreply.github.com>
2024-04-10 18:17:00 -0500

    week 4 notes

--- week4/divvy.js
+++ week4/divvy.js
@@ -2,14 +2,39 @@
 // Stations: https://gbfs.divvybikes.com/gbfs/en/station_information.json
 // Real-time status: https://gbfs.divvybikes.com/gbfs/en/station_status.json
 
-// document.addEventListener("DOMContentLoaded", function() {
-//   document.querySelector("h1").addEventListener("click", loadStations )
-// })
-
-// async function loadStations() {
-//   let http_response = await fetch("https://gbfs.divvybikes.com/gbfs/en/station_information.json")
-//   let station_data = await http_response.json()
-  
-//   console.log(station_data)
-
+document.addEventListener("DOMContentLoaded", function() {
+  document.querySelector("h1").addEventListener("click", loadStations )
+})
+// 法2
+// function emit_station(s) {
+//     console.log(s.name, s.capacity)
 // }
+async function loadStations() {
+    let http_response = await fetch("https://gbfs.divvybikes.com/gbfs/en/station_information.json")
+    let station_data = await http_response.json()
+  
+    console.log(station_data)
+
+    let stations = station_data.data.stations
+    // 法4
+    stations.forEach((s) => console.log(s.name, s.capacity))
+    
+//   // 法3
+//   stations.forEach((s) => {
+//     return console.log(s.name, s.capacity)
+//   })
+
+//   法2: anonymous function
+//   stations.forEach(function (s) {
+//     console.log(s.name, s.capacity)
+//   })
+
+
+//   法1：
+//   for (let i = 0; i < stations.length; ++i) {
+//     let station = stations[i]
+//     console.log(station.name, station.capacity)
+//   }
+
+
+}
--- week4/groceries.js
+++ week4/groceries.js
@@ -3,13 +3,35 @@
 // - the text input should be reset to a blank input field
 // - the cursor should be blinking in the text input again
 
+// 加一行为了能没有error
+// groceries.js:7 Uncaught TypeError: Cannot read properties of null (reading 'addEventListener')
+//     at setupListeners (groceries.js:7:33)
+//     at groceries.js:10:1
+
+document.addEventListener("DOMContentLoaded", setupListeners)
+
+
 function setupListeners() {
   document.querySelector("form").addEventListener("submit", addItem)
 }
 
-setupListeners();
+// 为了没有error要去掉
+// setupListeners();
+
 
 function addItem(e) {
   e.preventDefault();
   console.log(e);
+
+  let textToAdd = document.querySelector("input").value //把value设置成输入的
+  let template = document.querySelector("#new-item-template")
+  let newItem = template.content.cloneNode(true)
+  // 第一部分
+  // let newItem = document.createElement("li")
+  newItem.textContent = textToAdd
+  let ul = document.querySelector("#groceries") // 找到target的id
+  ul.appendChild(newItem)
+  
+  // 清空输入窗口
+  document.querySelector("input").value=""
 }
