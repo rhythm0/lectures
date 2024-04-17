70f8059435c6b5122c55f05a3fed18f1896def3f
Jeff Cohen <cohen.jeff@gmail.com>
2024-04-17 15:33:32 -0500

    At the break of Week 5

--- week5/movies/index.js
+++ week5/movies/index.js
@@ -1,4 +1,17 @@
-document.addEventListener("DOMContentLoaded", () => {
+window.app = { 
+  numberOfLikes: 0,
+}
 
+document.addEventListener("DOMContentLoaded", () => {
+  document.querySelectorAll(".like").forEach( (link) => { link.addEventListener("click", (event) => {
+    console.log("Clicked on a heart.")
+    const link = event.target;
+    const counterElement = link.querySelector("span")
+    // let counterValue = parseInt(counterElement.textContent)
+    // counterValue += 1
+    window.app.numberOfLikes += 1
+    counterElement.textContent = window.app.numberOfLikes
+    })  
+  })
 })
 
