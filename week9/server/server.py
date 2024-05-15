b4787df714d7925864a14893a74ee557a9e0b712
Jeff Cohen <cohen.jeff@gmail.com>
2024-05-15 10:34:40 -0500

    Ready for Week 9

---
+++ week9/server/server.py
@@ -0,0 +1,40 @@
+# Run this program: flask --app server run
+
+from flask import Flask
+from flask import render_template
+from flask import request
+
+app = Flask(__name__)
+
+
+@app.route("/") # 'root' path of the url
+def home():
+    return render_template('login_form.html')
+
+
+@app.route("/submit_login", methods=['POST'])
+def handle_login():
+    user = next((u for u in users() if u['email'] == request.form['email'] and u['password'] == request.form['password']), None)
+    if user:
+        return render_template('login_success.html')
+    
+    return render_template('login_failure.html')
+
+
+@app.route("/account", methods=["GET"])
+def show_account():
+    # Based on the user, look up their profile information
+    # SQL.....
+    user = users()[0]
+    return render_template("account.html", email=user['email'], password=user['password'], cc=user['cc'])
+
+
+def users():
+    return [
+        { 'id': 1, 'email': 'me@example.com', 'password': 'maroon', 'cc': "4111 2222 3333 9876   12/28  321" },
+        { 'id': 2, 'email': 'cookiemonster@example.com', 'password': 'cookies', 'cc': "5111 1234 3333 3541   5/27  789" },
+    ]
+
+# request.cookies.get(key)
+# resp = make_response(render_template(..)) 
+# resp.set_cookie('username', name) 
